f982a1e229b613b3a0eac8774c0151915dc973c7
jingchengpeng@yahoo.com <jingchengpeng@yahoo.com>
2022-12-23 09:14:26 -0400

    API testing finished

--- retrieve.py
+++ retrieve.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from time import sleep
 from concurrent.futures import ThreadPoolExecutor
 import tools
@@ -8,25 +8,36 @@ executor = ThreadPoolExecutor(2)
 app = Flask(__name__)
 
 
-@app.route('/jobs')
+@app.route('/getAvailability', methods=['GET'])
 def run_jobs():
-
-    while True:
-        executor.submit(some_long_task1(input("course"), input("code")))
-        isquit = input("quit?")
-        if isquit == 'q':
-            break
-    return 'running'
-
-
-def some_long_task1(course, code):
+    courseName = request.args.get('courseName')
+    courseCode = request.args.get('courseCode')
     try:
-        courseObj = mechanizeFuncs.getCourseSections(course, int(code))
-        hasSeat = tools.hasSeats(courseObj)
-        print(hasSeat)
-    except Exception as e:
-        print("Your course choice is not available next semester")
+        if(request.method == 'GET'):
+
+            courseObj = mechanizeFuncs.getCourseSections(
+                courseName, int(courseCode))
+            hasSeat = tools.hasSeats(courseObj)
+            data = {
+                "course": courseName+courseCode,
+                "hasSpot": hasSeat,
+
+            }
+            return jsonify(data)
+    except:
+        data = {
+            "course": courseName+courseCode,
+            "hasSpot": False,
+        }
+        return jsonify(data)
 
 
+# def some_long_task1(course, code):
+#     try:
+#         courseObj = mechanizeFuncs.getCourseSections(course, int(code))
+#         hasSeat = tools.hasSeats(courseObj)
+#         print(hasSeat)
+#     except Exception as e:
+#         print("Your course choice is not available next semester")
 if __name__ == '__main__':
     app.run()
